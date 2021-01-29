31b6b69a3bab3ba1a0617b3ca502f82f0d395e08
Weronika Jaworek <s18630@pjwstk.edu.pl>
2021-01-29 19:05:37 +0100

    Pobieranie danych lekarzy i lekarza

--- Cwiczenie11/Controllers/DoctorsController.cs
+++ Cwiczenie11/Controllers/DoctorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenie11.DTOs;
 using Cwiczenie11.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,26 @@ namespace Cwiczenie11.Controllers
         {
             return Ok(_context.Doctors.ToList());
         }
+        // pobierać dane lekarze, dodawać nowego lekarza, modyfikować dane lekarza i usuwać lekarza (4 końcówki).
 
+
+
+        [HttpPost]
+        public IActionResult GetDoctors(DoctorResponse response)
+        {
+
+
+            var res = _context.Doctors
+                      .Where(d => d.idDoctor == response.idDoctor)
+                      .FirstOrDefault();
+
+          
+            response.idDoctor = res.idDoctor;
+            response.FirstName = res.FirstName;
+            response.LastName = res.LastName;
+            response.Email = res.Email;
+
+            return Ok(response);
+        }
     }
 }
---
+++ Cwiczenie11/DTOs/DoctorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenie11.DTOs
+{
+    public class DoctorResponse
+    {
+
+
+        public int idDoctor { get; set; } // jak dodac ze to id
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
